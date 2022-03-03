36828f4e4512a51ab0bd8b1561dbe2198f0cefb7
Ravi kumar <ravikumar@Ravis-MacBook-Pro.local>
2022-03-03 10:23:53 -0500

    learning 01

---
+++ 01/datatype.js
@@ -0,0 +1,4 @@
+console.log(typeof name)
+console.log(typeof work)
+console.log(typeof time)
+console.log(typeof period)
---
+++ 01/variable.js
@@ -0,0 +1,10 @@
+let name = 'kiran'
+let work = 'learning'
+let time = 1
+const period = 3.14
+const varia = 'test\
+is\
+single\
+headed';
+
+console.log(name, work, time, period, varia)
